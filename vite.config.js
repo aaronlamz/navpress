81d80bdbaef7b1932b37ae092f5c5e14b58247c9
aaronlamz <aaronlamz2022@gmail.com>
2024-07-25 00:21:30 +0800

    fix build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'dist', // 输出目录
+    outDir: 'dist', // 确认输出目录
+    rollupOptions: {
+      input: 'public/index.html', // 指定入口文件
+    },
   },
 })
